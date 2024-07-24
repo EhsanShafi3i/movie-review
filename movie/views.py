8ed9ecf40461f480c589a513ff8378f6ed4ce0c4
ehsan shafiei <mr.ehsanshafiei82@gmail.com>
2024-07-24 11:27:04 +0330

    Implementing a search

--- movie/views.py
+++ movie/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
-
+from .models import Movie
 
 
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html',{'searchTerm':searchTerm})
-
-
+    if searchTerm:
+        movies = Movie.objects.filter(item_icontains=searchTerm)
+    else:
+        movies = movies.objects.all()
+    return render(request,'.home.html',context={"searchTerm":searchTerm,'movies':movies})
 def about(request):
     return HttpResponse("<h1>welcome to About page</h1>")
 
