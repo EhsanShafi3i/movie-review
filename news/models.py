a8825932b817eca26f47e39a636616f2c003ad07
ehsan shafiei <mr.ehsanshafiei82@gmail.com>
2024-07-24 19:48:54 +0330

    .

--- news/models.py
+++ news/models.py
@@ -5,3 +5,5 @@ class News(models.Model):
     headline=models.CharField(max_length=200)
     body=models.TextField()
     date=models.DateField()
+    def __str__(self):
+        return self.headline
