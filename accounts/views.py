64b8ec4c673b25e9ffbe2b27ba5c9f3cd60d3001
ehsan shafiei <mr.ehsanshafiei82@gmail.com>
2024-07-26 16:49:24 +0330

    sign up section

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def signupaccount(request):
+    return render(request, "signupaccount.html", {"form": UserCreationForm})
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("movie.urls")),
     path("news/",include("news.urls")),
+    path("accounts/",include("accounts.urls")),
     
 ]
